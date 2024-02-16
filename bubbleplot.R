29b4e7d37cc404391c7564250c7ac915e015b554
jeffock <jeffock1656@gmail.com>
2024-02-15 19:38:53 -0600

    commit

---
+++ bubbleplot.R
@@ -0,0 +1,11 @@
+# bubble plot w/ geom_point() from https://r-graph-gallery.com/bubble_chart_interactive_ggplotly.html
+
+# libs
+library(ggplot2)
+library(dplyr)
+library(viridis)
+library(hrbrthemes)
+
+# dataset
+library(gapminder)
+# ...
