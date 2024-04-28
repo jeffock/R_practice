0c6e50da5fe9b4a319cc9d52d283e9c91289cf6d
jeffock <jeffock1656@gmail.com>
2024-04-27 21:17:12 -0500

    update

--- linecharts.R
+++ linecharts.R
@@ -4,8 +4,8 @@
 vector <- c(1, 3, 6, 4, 9)
 
 # simple dot plot:
-plot(vector, type="o", col="blue")
-title(main="practice", col.main="blue", font.main=4)
+plot(vector, type = "o", col = "blue")
+title(main = "practice", col.main = "blue", font.main = 4)
 
 # open file
 # system('open Rplots.pdf')
@@ -17,15 +17,19 @@ names(csvdata)[names(csvdata) == "B"] <- "After"
 
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 library(hrbrthemes)
 hrbrthemes::import_roboto_condensed()
 
+# src: https://r-graph-gallery.com/histogram_several_group.html
+
 p <- csvdata %>%
-    ggplot( aes(x=value, fill=type)) +
-        geom_histogram( color="#e9ecef", alpha=0.6, position='identity') +
-        scale_fill_manual(values=c("#69b3a2", "#404080")) +
-        theme_ipsum() +
-        labs(fill="")
+    gather(key = "type", value = "value", Before, After) %>%
+    ggplot(aes(x=value, fill=type)) +
+    geom_histogram(binwidth = 1, color="#e9ecef", alpha=0.6, position='identity') +
+    scale_fill_manual(values=c("#69b3a2", "#404080")) +
+    theme_ipsum() +
+    labs(fill="")
 p
 
 library(htmlwidgets)
